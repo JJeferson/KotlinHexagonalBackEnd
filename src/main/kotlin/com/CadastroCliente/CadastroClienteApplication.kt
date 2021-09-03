72de12488c93c8f673e1823863d21415eaa5766d
JJeferson <48735241+JJeferson@users.noreply.github.com>
2021-09-03 17:01:51 -0300

    Commit 0

---
+++ src/main/kotlin/com/CadastroCliente/CadastroClienteApplication.kt
@@ -0,0 +1,11 @@
+package com.CadastroCliente
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CadastroClienteApplication
+
+fun main(args: Array<String>) {
+	runApplication<CadastroClienteApplication>(*args)
+}
---
+++ src/test/kotlin/com/CadastroCliente/CadastroClienteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.CadastroCliente
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CadastroClienteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
